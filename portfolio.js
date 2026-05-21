fa9fc7dc86b6c62020c7a6a209403444c8a55431
turnerkashton-dev <turnerkashton@gmail.com>
2026-05-21 07:54:43 -0600

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,50 @@
+
+
+
+function changeVideoSource(src, poster = 'images/all_card_1_up') {
+  // Update the video source and poster
+  videoPlayer.src = src;
+  videoPlayer.poster = poster;
+ 
+  classlist.add(hover-card-1)
+
+  // Load the new source and play
+  videoPlayer.load();
+  videoPlayer.play().catch(error => {
+    console.error("Auto-play failed:", error);
+    // Browser block auto-play without user interaction
+  });
+}
+
+
+
+
+// SCRIPTING FOR 2ND CLICK ON PACK WHEN IN pack1.html
+
+document.addEventListener("DOMContentLoaded", () => {
+  const secondImage = document.querySelector(".image-two-card-1");
+  const checkbox = document.querySelector(".image-toggler-card-1 input");
+
+  secondImage.addEventListener("click", () => {
+    // Only redirect if it's actually in the "checked" state
+    if (checkbox.checked) {
+      window.location.href = "cards1.html";
+    }
+  });
+});
+
+// SCRIPTING FOR 2ND CLICK ON PACK WHEN IN pack2.html
+
+document.addEventListener("DOMContentLoaded", () => {
+  const secondImage = document.querySelector(".image-two-card-2");
+  const checkbox = document.querySelector(".image-toggler-card-2 input");
+
+  secondImage.addEventListener("click", () => {
+    // Only redirect if it's actually in the "checked" state
+    if (checkbox.checked) {
+      window.location.href = "cards2.html";
+    }
+  });
+});
+
+
